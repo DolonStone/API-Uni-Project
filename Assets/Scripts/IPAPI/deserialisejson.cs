059568be97d3d4ffd3dc0a4979cd30d486ce017e
Beth C <147827249+beth-cl@users.noreply.github.com>
2024-11-27 17:29:05 +0000

    ipapi

--- Assets/Scripts/IPAPI/deserialisejson.cs
+++ Assets/Scripts/IPAPI/deserialisejson.cs
@@ -7,7 +7,10 @@ public class deserialisejson : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string json = getipapijson.json;
+        ipapijson ipresponse = JsonUtility.FromJson<ipapijson>(json);
+        Debug.Log(ipresponse.status);
+        Debug.Log(ipresponse.city);
     }
 
     // Update is called once per frame
