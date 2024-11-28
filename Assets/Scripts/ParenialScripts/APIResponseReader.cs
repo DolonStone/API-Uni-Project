23a1a3a1ac4b7716b42adc645460d908575be058
DolonStoned <up2203424@myport.ac.uk>
2024-11-28 13:28:09 +0000

    Fixed Search Bug

--- Assets/Scripts/ParenialScripts/APIResponseReader.cs
+++ Assets/Scripts/ParenialScripts/APIResponseReader.cs
@@ -17,7 +17,11 @@ public class APIResponseReader : MonoBehaviour
     }
     public void SearchAPI()
     {
-        
+        foreach (Transform child in scrollBarContent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        inputText.text = "";
         PerenualResponse plantresponse = APIHelper.GetPlant(inputText.text);
 
         for(int i = 0; i < plantresponse.data.Length; i++)
