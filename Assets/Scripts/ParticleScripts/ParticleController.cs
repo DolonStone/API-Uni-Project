380bf4815e68f73b52130fbf6d0d06339547b90a
Cid-er <willbear3@hotmail.com>
2024-12-19 13:13:29 +0000

    Adding Scripts for particle changes

---
+++ Assets/Scripts/ParticleScripts/ParticleController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public GameObject Particles;
+    void Start()
+    {
+        ComponentType particleSystem = Particles.GetComponent<Particle System>();
+    }
+
+}
